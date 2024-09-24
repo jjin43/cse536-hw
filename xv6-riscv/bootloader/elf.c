6cf3d00b2602c2abed3307adec9d3bd0258fdd7c
jjin43 <jjin43@asu.edu>
2024-09-24 00:54:09 -0700

    fixes

--- xv6-riscv/bootloader/elf.c
+++ xv6-riscv/bootloader/elf.c
@@ -11,45 +11,40 @@ struct elfhdr* kernel_elfhdr;
 struct proghdr* kernel_phdr;
 
 uint64 find_kernel_load_addr(enum kernel ktype) {
-    // Point an ELF struct to RAMDISK to initialize it with the kernel binary’s ELF header
+    // point elf struct
     if(ktype == NORMAL) {
         kernel_elfhdr = (struct elfhdr*)RAMDISK;
     } else if(ktype == RECOVERY) {
         kernel_elfhdr = (struct elfhdr*)RECOVERYDISK;
     }
 
-    // Retrieve the offset and size of the program headers
     uint64 phoff = kernel_elfhdr->phoff;
     uint64 phentsize = kernel_elfhdr->phentsize;
 
-    // Calculate the address of the second program header section
+    // calc addr of second program header section
     uint64 second_phdr_addr = RAMDISK + phoff + phentsize;
 
-    // Point a program header struct to this address to initialize it with the .text section’s header
     kernel_phdr = (struct proghdr*)second_phdr_addr;
+    uint64 kernel_text_addr = kernel_phdr->vaddr;
 
-    // Retrieve the starting address of the .text section
-    uint64 kernload_start = kernel_phdr->vaddr;
-
-    return kernload_start;
+    return kernel_text_addr;
 }
 
 uint64 find_kernel_size(enum kernel ktype) {
-    // Point an ELF struct to RAMDISK to initialize it with the kernel binary’s ELF header
     if(ktype == NORMAL) {
         kernel_elfhdr = (struct elfhdr*)RAMDISK;
     } else if(ktype == RECOVERY) {
         kernel_elfhdr = (struct elfhdr*)RECOVERYDISK;
     }
 
-    // Retrieve the number of program headers
+    // get num of program headers
     uint64 phnum = kernel_elfhdr->phnum;
     uint64 phoff = kernel_elfhdr->phoff;
     uint64 phentsize = kernel_elfhdr->phentsize;
 
     uint64 max_size = 0;
 
-    // Iterate through each program header to find the maximum of the p_offset + p_filesz values
+    // find the maximum of the p_offset + p_filesz values
     for (uint64 i = 0; i < phnum; i++) {
         kernel_phdr = (struct proghdr*)(RAMDISK + phoff + i * phentsize);
         uint64 size = kernel_phdr->off + kernel_phdr->filesz;
@@ -62,15 +57,12 @@ uint64 find_kernel_size(enum kernel ktype) {
 }
 
 uint64 find_kernel_entry_addr(enum kernel ktype) {
-    // Point an ELF struct to RAMDISK to initialize it with the kernel binary’s ELF header
     if(ktype == NORMAL) {
         kernel_elfhdr = (struct elfhdr*)RAMDISK;
     } else if(ktype == RECOVERY) {
         kernel_elfhdr = (struct elfhdr*)RECOVERYDISK;
     }
 
-    // Retrieve the entry point address from the ELF header
     uint64 entry_addr = kernel_elfhdr->entry;
-
     return entry_addr;
 }
--- xv6-riscv/bootloader/start.c
+++ xv6-riscv/bootloader/start.c
@@ -20,6 +20,7 @@ char *bl_stack_end = bl_stack + sizeof(bl_stack);
 /* Context (SHA-256) for secure boot */
 SHA256_CTX sha256_ctx;
 #define SYSINFOADDR 0x80080000
+#define bootloader_start = 0x80000000
 
 /* Structure to collects system information */
 struct sys_info {
@@ -113,9 +114,9 @@ void start()
   }
   
   /* CSE 536: Load the NORMAL kernel binary (assuming secure boot passed). */
-  uint64 kernel_load_addr       = find_kernel_load_addr(NORMAL);
-  uint64 kernel_binary_size     = find_kernel_size(NORMAL);     
-  uint64 kernel_entry           = find_kernel_entry_addr(NORMAL);
+  uint64 kernel_load_addr = find_kernel_load_addr(NORMAL);
+  uint64 kernel_binary_size = find_kernel_size(NORMAL);     
+  uint64 kernel_entry = find_kernel_entry_addr(NORMAL);
   
   struct buf b;
   uint64 num_blocks = kernel_binary_size / BSIZE;
@@ -123,6 +124,7 @@ void start()
     num_blocks++;
   }
 
+  // skip ELF header, first 4kb
   for (uint64 i = 4; i < num_blocks; i++) {
     b.blockno = i;
     kernel_copy(NORMAL, &b);
@@ -135,7 +137,7 @@ void start()
 
   /* CSE 536: Provide system information to the kernel. */
   sys_info_ptr = (struct sys_info*) SYSINFOADDR;
-  sys_info_ptr->bl_start = (uint64) start;
+  sys_info_ptr->bl_start = bootloader_start;
   sys_info_ptr->bl_end = (uint64) end;
   sys_info_ptr->dr_start = 0x80000000;
   sys_info_ptr->dr_end = 0x88000000;
