ca954aabe1124696ce5cedef0b8548844e000975
jjin43 <jjin43@asu.edu>
2024-11-06 23:34:45 -0700

    fixes

--- xv6-riscv/user/ulthread.c
+++ xv6-riscv/user/ulthread.c
@@ -49,7 +49,7 @@ int Priority(void) {
 
         if(all_thread[i].state == RUNNABLE) {
 
-            if(target == 0) {
+            if(target == -1) {
                 target = i;
 
             } else if(all_thread[i].priority < all_thread[target].priority) {
@@ -173,10 +173,15 @@ void ulthread_schedule(void) {
             printf("[DEBUG] Unexpected scheduling algorithm #.\n");
             break;
     }
+
+    if(next_index == -1) {
+        printf("[DEBUG] No thread to schedule.\n");
+        return;
+    }
     
     /* Add this statement to denote which thread-id is being scheduled next */
     printf("[*] ultschedule (next tid: %d)\n", all_thread[next_index].tid);
-    printf("[DEBUG] next_index: %d\n", next_index);
+    // printf("[DEBUG] next_index: %d\n", next_index);
 
     // Switch between thread contexts
     curr_thread = &all_thread[next_index];
