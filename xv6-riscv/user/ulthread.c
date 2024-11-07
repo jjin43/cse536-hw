d4e419f88c0d43427ade750e7ce1045ad0d0011b
jjin43 <jjin43@asu.edu>
2024-11-07 00:50:31 -0700

    fixes

--- xv6-riscv/user/ulthread.c
+++ xv6-riscv/user/ulthread.c
@@ -189,10 +189,11 @@ void ulthread_schedule(void) {
         }
         
         // Switch between thread contexts
+        struct ulthread_context temp = curr_thread->context;
         curr_thread = &all_thread[next_index];
         // printf("[schedule DEBUG] curr_thread->tid: %d, state:%d\n", curr_thread->tid, curr_thread->state);
         
-        ulthread_context_switch(&(all_thread[0].context), &(all_thread[next_index].context));
+        ulthread_context_switch(temp, &(all_thread[next_index].context));
     }
 }
 
