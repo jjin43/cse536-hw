95006c1b5f66049172f84e1c551db65292a913cf
jjin43 <jjin43@asu.edu>
2024-11-07 20:24:53 -0700

    debug

--- xv6-riscv/user/ulthread.c
+++ xv6-riscv/user/ulthread.c
@@ -200,6 +200,7 @@ void ulthread_schedule(void) {
         if(next_index == -1 && num_threads > 1) {
             // printf("[DEBUG] No thread to schedule.\n");
             if(target_state == RUNNABLE){
+                printf("[DEBUG] Looking for YIELD threads.\n");
                 next_index = Roundrobin(YIELD);
                 if(next_index == -1) {
                     printf("[DEBUG] No thread to schedule.\n");
